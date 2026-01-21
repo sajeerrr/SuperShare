b4de8417654a8a5f2b99b0619ef4fc9a8458b2cf
Sajeer <sajeerfm93@gmail.com>
2026-01-21 13:31:40 +0530

    server code 1 with download error msg

--- core/static/js/download.js
+++ core/static/js/download.js
@@ -5,3 +5,18 @@ history.pushState(null, null, location.href);
 window.addEventListener("popstate", function () {
     window.location.href = "/";
 });
+
+function handleDownload(code) {
+    const key = "downloaded_" + code;
+    const msg = document.getElementById("downloadMessage");
+
+    if (localStorage.getItem(key)) {
+        msg.style.display = "block";
+        return false; // Stop second download
+    }
+
+    // First time
+    localStorage.setItem(key, "true");
+    msg.style.display = "none";
+    return true; // Allow download
+}
